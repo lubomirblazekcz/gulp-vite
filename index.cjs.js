710033fb9cc374393e10da10615109c34b4a10c4
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2022-01-26 15:31:53 +0100

    feat: cjs build

--- index.cjs.js
+++ index.cjs.js
@@ -21,48 +21,50 @@ var index = new class {
             middleware: {
                 name: 'middleware',
                 apply: 'serve',
-                "configureServer": (viteDevServer) => {
+                configureServer: (viteDevServer) => {
                     return () => {
-                        viteDevServer.middlewares.use(async (context, res, next) => {
-                            if (!context.originalUrl.endsWith(".html") && context.originalUrl !== "/") {
-                                context.url = `/${this.options.output}` + context.originalUrl + ".html";
-                            } else if (context.url === "/index.html") {
+                        viteDevServer.middlewares.use(async(context, res, next) => {
+                            if (!context.originalUrl.endsWith('.html') && context.originalUrl !== '/') {
+                                context.url = `/${this.options.output}` + context.originalUrl + '.html';
+                            } else if (context.url === '/index.html') {
                                 context.url = `/${this.options.output}` + context.url;
                             }
 
                             next();
                         });
-                    };
+                    }
                 }
             },
             // reload page if there is change in public directory (it doesn't work with vite by default)
             reload: {
                 name: 'reload',
-                "handleHotUpdate": ({ file }) => {
-                    if ((!this.options.reloadPublic && !file.includes('.json') && !file.includes('.html') && file.includes(`/${this.options.output}/`))
-                        || (this.options.reloadPublic && file.includes(`/${this.options.output}/`)) || this.options.reloadFiles(file)) {
+                handleHotUpdate: ({ file }) => {
+                    if ((!this.options.reloadPublic && !file.includes('.json') && !file.includes('.html') && file.includes(`/${this.options.output}/`)) ||
+                        (this.options.reloadPublic && file.includes(`/${this.options.output}/`)) || this.options.reloadFiles(file)) {
                         this.reload(file);
                     }
                 }
             }
         }
     }
+
     reload(file) {
         // you can use this function to reload page in any gulp task you want, or anywhere in generally
-        if (typeof this.server !== "undefined") {
+        if (typeof this.server !== 'undefined') {
             this.server.ws.send({
                 type: 'full-reload',
-                path: '*',
+                path: '*'
             });
             this.server.config.logger.info(
-                chalk__default["default"].green(`page reload `) + chalk__default["default"].dim(typeof file !== "undefined" ? file.replace(`${this.options.root}/`, "") : `*.html`),
+                chalk__default["default"].green('page reload ') + chalk__default["default"].dim(typeof file !== 'undefined' ? file.replace(`${this.options.root}/`, '') : '*.html'),
                 { clear: true, timestamp: true }
             );
         }
     }
+
     init(userOptions = {}) {
-        let options = {
-            output: "public",
+        const options = {
+            output: 'public',
             root: process.cwd(),
             ignored: [],
             reloadPublic: true,
@@ -71,12 +73,12 @@ var index = new class {
 
         lodash__default["default"].merge(options, userOptions);
 
-        let viteOptions = {
+        const viteOptions = {
             vite: {
                 plugins: [this.plugin.middleware, this.plugin.reload],
                 publicDir: path__default["default"].join(options.root, options.output),
                 server: {
-                    open: "/",
+                    open: '/',
                     host: true,
                     fsServe: {
                         strict: false
@@ -105,13 +107,13 @@ var index = new class {
             // starts the server
             await this.server.listen();
 
-            console.log(chalk__default["default"].cyan(`\n  vite v${require$1('vite/package.json').version}`) + chalk__default["default"].green(` dev server running at:\n`));
+            console.log(chalk__default["default"].cyan(`\n  vite v${require$1('vite/package.json').version}`) + chalk__default["default"].green(' dev server running at:\n'));
             this.server.printUrls();
-            console.log("");
+            console.log('');
 
             resolve();
         })
     }
-};
+}();
 
 module.exports = index;
