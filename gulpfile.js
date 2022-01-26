26d0e26d88646481d6527339b13890d397407a85
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2022-01-26 15:58:50 +0100

    feat: gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,51 @@
-import gulp from "gulp";
-import esbuild from "gulp-esbuild";
-import twig from "gulp-twig2html";
-import rename from "gulp-rename";
-import fs from "fs";
-import path from "path";
-import lodash from "lodash";
-import postCssPlugin from "esbuild-plugin-postcss2";
-import Serve from "./index.mjs";
+import gulp from 'gulp'
+import esbuild from 'gulp-esbuild'
+import twig from 'gulp-twig2html'
+import rename from 'gulp-rename'
+import fs from 'fs'
+import path from 'path'
+import lodash from 'lodash'
+import postCssPlugin from 'esbuild-plugin-postcss2'
+import Serve from './index.mjs'
 
-let config = {
+const mergeConfig = async (config, path) => {
+    if (fs.existsSync(path)) {
+        lodash.merge(config, (await import(path)).default)
+    }
+}
+
+const configPath = path.join(process.cwd(), "gulpfile.config.js")
+
+const config = {
     root: process.cwd(),
     output: {
-        dir: "public",
-        scripts: "public/assets",
-        styles: "public/assets",
-        templates: "public"
+        dir: 'public',
+        scripts: 'public/assets',
+        styles: 'public/assets',
+        templates: 'public'
     },
     input: {
-        dir: "src",
-        main: "src/main.json",
-        scripts: "src/scripts",
-        styles: "src/styles",
-        templates: "src/templates"
+        dir: 'src',
+        main: 'src/main.json',
+        scripts: 'src/scripts',
+        styles: 'src/styles',
+        templates: 'src/templates'
     },
     serve: {
-        mode: ""
+        mode: ''
     },
     styles: {
         postcss: []
     }
 }
 
-let userConfig = {};
+await mergeConfig(config, configPath)
 
-let viteOptions = {
+const serve = {
     ignored: [`**/${config.input.templates}/**`],
     output: config.output.templates,
     reloadPublic: false,
-    reloadFiles: (file) => file.endsWith(".php"),
+    reloadFiles: (file) => file.endsWith('.php'),
     vite: {
         css: {
             postcss: {
@@ -47,11 +55,9 @@ let viteOptions = {
     }
 }
 
-if (fs.existsSync(path.join(process.cwd(), "gulpfile.config.js"))) {
-    userConfig = (await import(path.join(process.cwd(), "gulpfile.config.js"))).default;
-}
+lodash.merge( config, { serve })
 
-lodash.merge(config, userConfig);
+await mergeConfig(config, configPath)
 
 const exists = {
     scripts: fs.existsSync(path.join(config.root, config.input.scripts)),
@@ -70,12 +76,12 @@ const Scripts = new class {
                 bundle: true,
                 minify: true,
                 assetNames: '[name].[hash]',
-                chunkNames: '[name].[hash]',
+                chunkNames: '[name].[hash]'
                 // entryNames: '[name].[hash]'
             }))
-            .pipe(gulp.dest(path.join(config.root, config.output.scripts)));
+            .pipe(gulp.dest(path.join(config.root, config.output.scripts)))
     }
-}
+}()
 
 const Styles = new class {
     build() {
@@ -88,33 +94,34 @@ const Styles = new class {
                         plugins: config.styles.postcss
                     })
                 ],
-                minify: true,
+                minify: true
                 // entryNames: '[name].[hash]'
             }))
-            .pipe(gulp.dest(path.join(config.root, config.output.styles)));
+            .pipe(gulp.dest(path.join(config.root, config.output.styles)))
     }
-}
+}()
 
 const Templates = new class {
     get filters() {
         return {
-            "asset": (url) => {
-                if (config.serve.mode === "dev" && url.indexOf("/" + config.input.dir) === 0 || url.includes("https://") || url.includes("http://")) {
-                    return url;
+            asset: (url) => {
+                if (config.serve.mode === 'dev' && url.indexOf('/' + config.input.dir) === 0 || url.includes('https://') || url.includes('http://')) {
+                    return url
                 }
 
-                if (config.serve.mode !== "dev" && url.indexOf("/" + config.input.dir) === 0) {
+                if (config.serve.mode !== 'dev' && url.indexOf('/' + config.input.dir) === 0) {
                     url = url
                         .replace(`/${config.input.styles}`, `/${config.output.styles}`)
                         .replace(`/${config.input.scripts}`, `/${config.output.scripts}`)
                 }
 
-                url = url.replace(`/${config.output.dir}`, "")
+                url = url.replace(`/${config.output.dir}`, '')
 
-                return url;
+                return url
             }
         }
     }
+
     build() {
         return gulp.src(config.root + `/${config.input.templates}/*.twig`)
             .pipe(twig({
@@ -127,60 +134,60 @@ const Templates = new class {
             }))
             .pipe(rename({ extname: '.html' }))
             .pipe(gulp.dest(path.join(config.root, config.output.templates)))
-            .on("end", () => Serve.reload(`${config.output.templates}/*.html`))
+            .on('end', () => Serve.reload(`${config.output.templates}/*.html`))
     }
-}
+}()
 
 // Everything is loaded from input path with vite, only templates are being build from sources
-gulp.task("serve", resolve => {
-    let tasks = [];
+gulp.task('serve', resolve => {
+    const tasks = []
 
-    config.serve.mode = "dev";
+    config.serve.mode = 'dev'
 
-    exists.templates && tasks.push("templates")
+    exists.templates && tasks.push('templates')
 
-    tasks.push(() => Serve.init(viteOptions), "watch")
+    tasks.push(() => Serve.init(config.serve), 'watch')
 
     gulp.series(tasks)(resolve)
-});
+})
 
 // Everything is loaded from output path with vite, styles, scripts and templates are being build from sources
-gulp.task("serve:build", resolve => {
-    let tasks = [];
+gulp.task('serve:build', resolve => {
+    const tasks = []
 
-    config.serve.mode = "build";
+    config.serve.mode = 'build'
 
-    exists.scripts && tasks.push("scripts:build")
-    exists.styles && tasks.push("styles:build")
-    exists.templates && tasks.push("templates")
+    exists.scripts && tasks.push('scripts:build')
+    exists.styles && tasks.push('styles:build')
+    exists.templates && tasks.push('templates')
 
-    tasks.push(() => Serve.init(viteOptions), "watch:build")
+    tasks.push(() => Serve.init(config.serve), 'watch:build')
 
     gulp.series(tasks)(resolve)
-});
+})
 
-gulp.task("scripts:build", () => Scripts.build());
+gulp.task('scripts:build', () => Scripts.build())
 
-gulp.task("styles:build", () => Styles.build());
+gulp.task('styles:build', () => Styles.build())
 
-gulp.task("templates", () => Templates.build());
+gulp.task('templates', () => Templates.build())
 
-gulp.task("watch", () => {
+gulp.task('watch', () => {
     if (exists.templates) {
-        gulp.watch(`${config.root}/${config.input.templates}/**`, gulp.series("templates"))
+        gulp.watch(`${config.root}/${config.input.templates}/**`, gulp.series('templates'))
     }
 })
 
-gulp.task("watch:build", () => {
+gulp.task('watch:build', () => {
     if (exists.scripts) {
-        gulp.watch(`${config.root}/${config.input.scripts}/**`, gulp.series("scripts:build"))
+        gulp.watch(`${config.root}/${config.input.scripts}/**`, gulp.series('scripts:build'))
     }
 
     if (exists.styles) {
-        gulp.watch(`${config.root}/${config.input.styles}/**`, gulp.series("styles:build"))
+        gulp.watch(`${config.root}/${config.input.styles}/**`, gulp.series('styles:build'))
     }
 
     if (exists.templates) {
-        gulp.watch(`${config.root}/${config.input.templates}/**`, gulp.series("templates"))
+        gulp.watch(`${config.root}/${config.input.templates}/**`, gulp.series('templates'))
     }
 })
